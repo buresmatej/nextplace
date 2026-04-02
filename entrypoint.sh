0f4c702db7a82d6aef48d41627a01aa2d7d94891
Matěj Bureš <matejbures11011@gmail.com>
2026-04-02 12:41:21 +0200

    x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -ex
 
-# Příprava složek v jediném povoleném místě
 mkdir -p /tmp/nette_temp /tmp/nette_log
 chmod -R 777 /tmp
-
-# PHP log
 touch /tmp/php-fpm.log && chmod 777 /tmp/php-fpm.log
+
+# Explicitně předej proměnné do PHP-FPM poolu
+echo "env[OPENAI_BASE_URL] = $OPENAI_BASE_URL" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[OPENAI_API_KEY] = $OPENAI_API_KEY" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[AI_MODEL] = $AI_MODEL" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[PORT] = $PORT" >> /usr/local/etc/php-fpm.d/www.conf
 
 echo "Waiting for Postgres..."
 until php -r "new PDO('pgsql:host=db;port=5432;dbname=nette_db', 'root', 'root');" 2>/dev/null; do
@@ -14,9 +17,7 @@ until php -r "new PDO('pgsql:host=db;port=5432;dbname=nette_db', 'root', 'root')
   sleep 2
 done
 
-
 echo "Postgres ready, executing migrations..."
 php bin/console migrations:reset --no-interaction
 
-# Spuštění FPM (příznak -R povolí běh pod rootem v configu)
 exec php-fpm -R
